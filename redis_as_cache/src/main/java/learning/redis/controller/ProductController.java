96aa7fe622229f8958ccff5a9ad9a4f85025f148
sanjay <rockysanjay287@gmail.com>
2025-06-06 15:18:42 +0530

    learnt to use redis as cache

---
+++ redis_as_cache/src/main/java/learning/redis/controller/ProductController.java
@@ -0,0 +1,44 @@
+package learning.redis.controller;
+
+import learning.redis.entity.Product;
+import learning.redis.repository.ProductDao;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    private final ProductDao dao;
+
+    public ProductController(
+            ProductDao productDao
+    ){
+        this.dao=productDao;
+    }
+
+    @PostMapping
+    public Product save(@RequestBody Product product) {
+        return dao.save(product);
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return dao.findAll();
+    }
+
+    @GetMapping("/{id}")
+    @Cacheable(key="#id", value = "Product", unless = "#result.price>1000")
+    public Product findProduct(@PathVariable int id) {
+        return dao.findProductById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @CacheEvict(key = "#id",value = "Product")
+    public String remove(@PathVariable int id)   {
+        return dao.deleteProduct(id);
+    }
+}
---
+++ redis_as_cache/src/main/java/learning/redis/repository/ProductDao.java
@@ -0,0 +1,41 @@
+package learning.redis.repository;
+
+import learning.redis.entity.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class ProductDao {
+
+    public static final String HASH_KEY = "Product"; // same as the value in RedisHash on top of Product entity
+    private final RedisTemplate template;
+
+    public ProductDao(
+            RedisTemplate template
+    ){
+        this.template=template;
+    }
+
+    public Product save(Product product){
+        template.opsForHash().put(HASH_KEY,product.getId(),product);
+        return product;
+    }
+
+    public List<Product> findAll(){
+        return template.opsForHash().values(HASH_KEY);
+    }
+
+    public Product findProductById(int id){
+        System.out.println("Called findProductById() from DB");
+        return (Product) template.opsForHash().get(HASH_KEY,id);
+    }
+
+
+    public String deleteProduct(int id){
+        template.opsForHash().delete(HASH_KEY,id);
+        return "product removed !!";
+    }
+}
